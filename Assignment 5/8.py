6c45546e12f48b61771a49f8254db799af18d0f8
gautamabhinav <55458871+gautamabhinav@users.noreply.github.com>
2023-06-30 18:33:52 +0530

    Update 8.py

--- Assignment 5/8.py
+++ Assignment 5/8.py
@@ -1,18 +1,33 @@
 from collections import Counter
 
-def find_original(changed):
-    count = Counter(changed)
+def findOriginalArray(changed):
+    # Check if the length of the changed array is odd
+    if len(changed) % 2 != 0:
+        return []
 
+    # Count the frequency of each element in the changed array
+    freq = Counter(changed)
+
+    # Sort the changed array in ascending order
+    changed.sort()
+
+    # Construct the original array
+    original = []
     for num in changed:
-        doubled_num = num * 2
-        if doubled_num not in count or count[doubled_num] == 0:
+        # Check if the doubled value is available in the frequency counter
+        if freq[num] > 0 and freq[num * 2] > 0:
+            original.append(num)
+            freq[num] -= 1
+            freq[num * 2] -= 1
+
+    # Check if all elements have been used in the original array
+    for count in freq.values():
+        if count > 0:
             return []
 
-        count[doubled_num] -= 1
-
-    return list(count.keys())
+    return original
 
 # Test case
 changed = [1, 3, 4, 2, 6, 8]
-original = find_original(changed)
+original = findOriginalArray(changed)
 print(original)
